adb5dcf3538a2944ca788e9b2c9b028489c02ccb
Gabriel BDCP <gabrielbotana@gmail.com>
2025-01-28 22:19:00 +0100

    First test to complete

---
+++ ts/leap-year/leap-years.test.ts
@@ -0,0 +1,15 @@
+
+describe('leap years', () => {
+
+  const leapYearsDivisibleBy400 = [2000, 2400, 2800]
+
+  it('All years divisible by 400 ARE leap years', () => {
+    leapYearsDivisibleBy400.forEach(year => {
+
+      // TODO: implement leap year logic
+      const result = true
+      expect(result).toBe(true)
+    })
+  })
+
+})
