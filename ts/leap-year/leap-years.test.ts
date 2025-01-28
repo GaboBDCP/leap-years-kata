10d4be4fa4cc143264148a27ff84fa6d48ffe339
Gabriel BDCP <gabrielbotana@gmail.com>
2025-01-28 22:55:04 +0100

    Add final logic

--- ts/leap-year/leap-years.test.ts
+++ ts/leap-year/leap-years.test.ts
@@ -3,6 +3,9 @@ import { isLeapYear } from "./leap-year"
 describe('leap years', () => {
 
   const leapYearsDivisibleBy400 = [2000, 2400, 2800]
+  const leapYearsDivisibleBy4ButNotBy100 = [2008, 2012, 2016]
+  const leapYearsNotDivisibleBy4 = [2017, 2018, 2019]
+  const leapYearsDivisibleBy100ButNotBy400 = [1700, 1800, 1900]
 
   it('All years divisible by 400 ARE leap years', () => {
     leapYearsDivisibleBy400.forEach(year => {
@@ -14,15 +17,24 @@ describe('leap years', () => {
   })
 
   it('All years divisible by 100 but not by 400 are NOT leap years', () => {
-    const year = 1900
-    const result = isLeapYear(year)
-    expect(result).toBe(false)
+    leapYearsDivisibleBy100ButNotBy400.forEach(year => {
+      const result = isLeapYear(year)
+      expect(result).toBe(false)
+    })
   })
 
   it('All years divisible by 4 but not by 100 ARE leap years', () => {
-    const year = 2008
-    const result = isLeapYear(year)
-    expect(result).toBe(true)
+    leapYearsDivisibleBy4ButNotBy100.forEach(year => {
+      const result = isLeapYear(year)
+      expect(result).toBe(true)
+    })
+  })
+
+  it('All years not divisible by 4 are NOT leap years', () => {
+    leapYearsNotDivisibleBy4.forEach(year => {
+      const result = isLeapYear(year)
+      expect(result).toBe(false)
+    })
   })
 
 })
